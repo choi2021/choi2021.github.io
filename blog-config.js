b4e2be4391cf94d0879fc1512f496789c44c70cd
Youngjun Choi <juni2004c@gmail.com>
2024-02-25 22:59:34 +0900

    Initial commit from gatsby: (https://github.com/devHudi/gatsby-starter-hoodie.git)

---
+++ blog-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  title: "Troy DevLog",
+  description: "안녕하세요 주도적으로 성장하는 개발자, Troy입니다.",
+  author: "Troy",
+  siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
+  links: {
+    github: "https://github.com/choi2021",
+    linkedIn: "https://www.linkedin.com/in/%EC%98%81%EC%A4%80-%EC%B5%9C-3a316825a/",
+    // facebook: "https://www.facebook.com",
+    // twitter: "https://twitter.com",
+    // x: "https://x.com",
+    // instagram: "https://www.instagram.com",
+    // medium: "https://medium.com",
+    // blogger: "https://blogger.com",
+    email: "mailto:juni2004c@gmail.com",
+    resume: "https://hudi.blog",
+    link: "https://hudi.blog",
+  },
+  utterances: {
+    repo: "devHudi/gatsby-starter-hoodie",
+    type: "pathname",
+  },
+}
